b37046459c5d09d42a4a74c406c31043aab71563
Radek <225951@student.pwr.edu.pl>
2019-11-21 02:18:31 +0100

    Added enemies and goal of the game

--- game.js
+++ game.js
@@ -14,34 +14,48 @@ let holdLeft = false;
 let jumping = false;
 const gravity = 0.6;
 const friction = 0.9;
-let jmpVar = 0; // zmienna która sprawia że klocek nie odlatuje, tymaczasowe rozwiązanie
-player = {
+let win = 0;
+let jmpVar = 0; 
+let enemySpeed = 4;
+let defeat = 3;
+let player = {
   x: cw / 2,
   y: ch - 20,
-  width: 10,
-  height: 10,
+  width: 15,
+  height: 15,
   speed: 3,
   speedX: 0,
   speedY: 0,
   onGround: false
 };
+let goal = {
+  x: Math.random() * cw,
+  y: ch - 300,
+  width: 10,
+  height: 10,
+};
+const enemy = {
+  x: 50,
+  y: ch - 15,
+  width: 15,
+  height: 15,
+};
 
 let platforms = []
 for (i = 0; i < 15; i++) {
   platforms.push({
-    x: Math.random() * canvas.width,
-    y: Math.random() * canvas.height,
+    x: Math.random() * cw,
+    y: Math.random() * ch - 60,
     width: Math.random() * 100 + 40,
     height: 10
   });
 }
 
-
 function update() {
-
 
   document.addEventListener("keydown", keyDown);
   document.addEventListener("keyup", keyUp);
+
   if (holdLeft == true) {
     if (player.speedX > -player.speed) {
       player.speedX--;
@@ -66,6 +80,14 @@ function update() {
   player.x += player.speedX;
   player.y += player.speedY;
 
+  enemy.x += enemySpeed;
+  if (enemy.x >= cw - enemy.width) {
+    enemySpeed = -4;
+  } else if (enemy.x <= 0) {
+    enemySpeed = 4;
+  }
+
+
   if (player.x >= cw - player.width) {
     player.x = cw - player.width;
   } else if (player.x <= 0) {
@@ -83,11 +105,9 @@ function update() {
   ctx.beginPath();
 
   player.onGround = false;
-  for (var i = 0; i < platforms.length; i++) {
+  for (let i = 0; i < platforms.length; i++) {
     ctx.rect(platforms[i].x, platforms[i].y, platforms[i].width, platforms[i].height);
-
-    var dir = colCheck(player, platforms[i]);
-
+    let dir = colCheck(player, platforms[i]);
     if (dir === "left" || dir === "right") {
       player.speedX = 0;
       jumping = false;
@@ -100,16 +120,22 @@ function update() {
       player.speedY *= -1;
     }
   }
-
-  if(player.onGround){
+  if (player.onGround) {
     player.speedY = 0;
-}
+  }
 
+  goalCheck(player, goal);
+  enemyCheck(player, enemy);
 
   ctx.fill();
-
   ctx.fillStyle = '#afdcfa';
   ctx.fillRect(player.x, player.y, player.width, player.height);
+
+  ctx.fillStyle = '#fffb00';
+  ctx.fillRect(goal.x, goal.y, goal.width, goal.height);
+
+  ctx.fillStyle = '#ff0000';
+  ctx.fillRect(enemy.x, enemy.y, enemy.width, enemy.height);
 
   requestAnimationFrame(update);
 }
@@ -120,8 +146,8 @@ function keyDown(event) {
       holdLeft = true;
       break;
     case 38:
-        jumping = true;
-        player.onGround = false;
+      jumping = true;
+      player.onGround = false;
       break;
     case 39:
       holdRight = true;
@@ -144,14 +170,14 @@ function keyUp(event) {
 }
 
 function colCheck(shapeA, shapeB) {
-  var vX = (shapeA.x + (shapeA.width / 2)) - (shapeB.x + (shapeB.width / 2)),
+  let vX = (shapeA.x + (shapeA.width / 2)) - (shapeB.x + (shapeB.width / 2)),
     vY = (shapeA.y + (shapeA.height / 2)) - (shapeB.y + (shapeB.height / 2)),
     hWidths = (shapeA.width / 2) + (shapeB.width / 2),
     hHeights = (shapeA.height / 2) + (shapeB.height / 2),
     colDir = null;
 
   if (Math.abs(vX) < hWidths && Math.abs(vY) < hHeights) {
-    var oX = hWidths - Math.abs(vX),
+    let oX = hWidths - Math.abs(vX),
       oY = hHeights - Math.abs(vY);
     if (oX >= oY) {
       if (vY > 0) {
@@ -174,7 +200,42 @@ function colCheck(shapeA, shapeB) {
   return colDir;
 }
 
+function goalCheck(shapeA, shapeB) {
+  if (win == 3) {
+    if (!alert("You won")) {
+      window.location.reload();
+    }
+  } else if (colCheck(shapeA, shapeB) !== null) {
+    win++;
+    goal = {
+      x: Math.random() * cw,
+      y: ch - 300,
+      width: 10,
+      height: 10,
+    }
+  }
+}
 
+function enemyCheck(shapeA, shapeB) {
+  if (defeat == 0) {
+    if (!alert("You lost")) {
+      window.location.reload();
+    }
+  } else if (colCheck(shapeA, shapeB) !== null) {
+    defeat--;
+    document.getElementById("lifes").innerText = `lifes: ${defeat}`;
+    player = {
+      x: cw / 2,
+      y: ch - 100,
+      width: 15,
+      height: 15,
+      speed: 3,
+      speedX: 0,
+      speedY: 0,
+      onGround: false
+    }
+  }
+}
 
 window.addEventListener("load", function () {
   update();
