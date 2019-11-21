4a29ca88c4a085383da2713c405b9e61a9e021d5
Radek <225951@student.pwr.edu.pl>
2019-11-21 04:19:26 +0100

    dummy commits to fix github pages

--- game.js
+++ game.js
@@ -5,7 +5,7 @@
 
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-canvas.width = 1000;
+canvas.width = 800;
 canvas.height = 500;
 const cw = canvas.width;
 const ch = canvas.height;
